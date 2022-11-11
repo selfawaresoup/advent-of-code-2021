e10ed1c45c4fb41efe72fc98304badf75bfae4b6
Esther Weidauer <esther@selfawaresoup.com>
2022-11-11 22:59:29 +0100

    template change

--- src/_template.rs
+++ src/_template.rs
@@ -2,7 +2,7 @@ static INPUT:&str = include_str!("day-02-input");
 
 fn get_input() -> () {
 	let input = String::from(INPUT);
-	let lines = input.split("\n");
+	let entries = input.split("\n");
 
 }
 
